082b8c86e7f8c1709d588e7683c402eef1f6ea16
Janek <janbarn00@gmail.com>
2025-07-04 20:13:45 +0200

    added success alert when created todo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -74,6 +74,12 @@
             <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
         </div>
     </header>
+
+    @if(session('success'))
+        <x-success-alert>
+        </x-success-alert>
+    @endif
+
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             {{$slot}}
---
+++ resources/views/components/success-alert.blade.php
@@ -0,0 +1,14 @@
+<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="mt-4 rounded-md bg-green-50 p-4">
+        <div class="flex">
+            <div class="shrink-0">
+                <svg class="size-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.236 4.53L8.53 10.53a.75.75 0 0 0-1.06 1.061l2.03 2.03a.75.75 0 0 0 1.137-.089l3.857-5.401Z" clip-rule="evenodd" />
+                </svg>
+            </div>
+            <div class="ml-3">
+                <p class="text-sm font-medium text-green-800">{{ session('success') }}</p>
+            </div>
+        </div>
+    </div>
+</div>
