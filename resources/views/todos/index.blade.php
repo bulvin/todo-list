d1719a227cce6e7e455b86f058f7617fa6099761
Janek <janbarn00@gmail.com>
2025-07-04 18:35:15 +0200

    removed hardcoded statuses from todo list view

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -3,17 +3,11 @@
         To-Do List
     </x-slot:heading>
     <div class="flex flex-wrap gap-6 items-start">
-        <x-todo-column
-            title="To-Do"
-            :todos="$todos->where('status', 'todo')"
-        />
-        <x-todo-column
-            title="In Progress"
-            :todos="$todos->where('status', 'in_progress')"
-        />
-        <x-todo-column
-            title="Done"
-            :todos="$todos->where('status', 'done')"
-        />
+        @foreach(\App\Enums\TodoStatus::cases() as $status)
+            <x-todo-column
+                :title="$status->label()"
+                :todos="$todos->where('status', $status->value)"
+            />
+        @endforeach
     </div>
 </x-layout>
